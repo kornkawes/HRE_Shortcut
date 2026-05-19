8606a9475757577896ad88e0758a87576fdc010d
VerveOrders <kornkawes@gmail.com>
2026-05-19 17:26:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = 'home-re-v2';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = "hre-shortcut-v2";
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => undefined))
-  );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
-      return response;
-    }).catch(() => cached))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
